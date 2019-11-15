943608f75e0ccf66ca9abc1c51840eca38e2609b
theabgupta <abgupta.456@gmail.com>
2019-11-15 15:39:31 +0530

    grammar articles error

--- new/ksl/ld/src/components/Sections/HelloSection/HelloSection.jsx
+++ new/ksl/ld/src/components/Sections/HelloSection/HelloSection.jsx
@@ -13,7 +13,7 @@ class Hello extends React.Component {
             <div className="row">
               <div className="col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1 text-center">
                 <h2 className="title">Abhishek Gupta</h2>
-                <h4 className="subtitle">Just a IT Guy</h4>
+                <h4 className="subtitle">Just an IT Guy</h4>
                 <h5 className="tagline">
                   <sup><i className="fa fa-quote-left fa-lg"></i></sup>  Put your heart, mind, and soul into even your smallest acts.<br></br>
                   This is the secret of success.  <sup><i className="fa fa-quote-right fa-lg"></i></sup>
